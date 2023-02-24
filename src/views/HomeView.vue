2b5c917dbed867e1765ab548feb09a00bae652d8
QuentinHeinis <quentin@heinis.dev>
2023-02-24 09:23:51 +0100

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="flex flex-col">
+    <span class="flex">Web design</span>
+    <span class="flex">Graphic d<img src="" />sign</span>
+    <span class="flex">Log <img src="" />s</span>
+    <span class="flex">Product</span>
+  </h1>
+</template>
